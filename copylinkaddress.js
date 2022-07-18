dab60a622b32a25da6aa81ef5d1ac3c592786f28
lowking <lowking@sina.cn>
2022-07-18 16:07:58 +0800

    fix: show link span on top right when link not starts with http and format code

--- copylinkaddress.js
+++ copylinkaddress.js
@@ -16,85 +16,90 @@ When you move away from the link, the caret position is restored.
 
 */
 
-var linkAddress = $('<span id="copylAddress" style="display: inline-block;" />');
-$('body').append(linkAddress);
+let linkAddress = $('<span id="copylAddress" style="display: inline-block;" />')
+$('body').append(linkAddress)
 // This is a DOM element that has to be selectable but not visible to anybody
-linkAddress.css({position: 'absolute', left:'-9999em'});
+linkAddress.css({position: 'fixed', top: '0em', right: '0em'})
 
-var previousCaretPosition = -1;
+let previousCaretPosition = -1
 
-COPYL_DEBUG = false;
+COPYL_DEBUG = false
 
 function write_to_console(text) {
     if (COPYL_DEBUG)
-        console.log(text);
+        console.log(text)
 }
 
 function selectElement(el) {
     // Check if anything is currently selected, if so backup
     if (window.getSelection().rangeCount > 0) {
-        previousCaretPosition = document.activeElement.selectionStart;
+        previousCaretPosition = document.activeElement.selectionStart
     }
-    write_to_console("Previous cursor position: " + previousCaretPosition);
-    var range = document.createRange();
+    write_to_console("Previous cursor position: " + previousCaretPosition)
+    var range = document.createRange()
 
-    write_to_console("Selecting element " + el);
-    write_to_console("el[0] is " + el[0]);
-    range.selectNodeContents(el[0]);
-    write_to_console("Range: " + range);
+    write_to_console("Selecting element " + el)
+    write_to_console("el[0] is " + el[0])
+    range.selectNodeContents(el[0])
+    write_to_console("Range: " + range)
 
-    write_to_console("Window selection range count before: " + window.getSelection().rangeCount);
+    write_to_console("Window selection range count before: " + window.getSelection().rangeCount)
     if (window.getSelection().rangeCount > 0) {
-        window.getSelection().removeAllRanges();
+        window.getSelection().removeAllRanges()
     }
-    write_to_console("Window selection range count now: " + window.getSelection().rangeCount);
+    write_to_console("Window selection range count now: " + window.getSelection().rangeCount)
 
-    write_to_console("Active Element: " + document.activeElement);
-    window.getSelection().addRange(range);
-    write_to_console("Window selection range count after: " + window.getSelection().rangeCount);
+    write_to_console("Active Element: " + document.activeElement)
+    window.getSelection().addRange(range)
+    write_to_console("Window selection range count after: " + window.getSelection().rangeCount)
 }
 
 function clearLinkAddress() {
     if (linkAddress.text()) {
-        linkAddress.text("");
-        linkAddress.blur();
-        write_to_console("Cleared linkAddress");
+        linkAddress.text("")
+        linkAddress.blur()
+        write_to_console("Cleared linkAddress")
 
-        write_to_console("window.getSelection: " + window.getSelection());
-        window.getSelection().removeAllRanges();
+        write_to_console("window.getSelection: " + window.getSelection())
+        window.getSelection().removeAllRanges()
     }
-    if (previousCaretPosition != -1)
-    {
-        document.activeElement.selectionStart = previousCaretPosition;
-        write_to_console("Previous cursor position set: " + document.activeElement.selectionStart);
+    if (previousCaretPosition !== -1) {
+        document.activeElement.selectionStart = previousCaretPosition
+        write_to_console("Previous cursor position set: " + document.activeElement.selectionStart)
     }
 
-    write_to_console("Current selection: " + window.getSelection().toString());
+    write_to_console("Current selection: " + window.getSelection().toString())
 }
 
-$(function() {
+$(function () {
     // The code attaches itself to all anchor elements
-    $("html").on("mouseenter", "a", function(){
+    $("html").on("mouseenter", "a", function () {
         // Everytime the user hovers (enters) a link
-        if(window.getSelection().toString()) {
-            write_to_console("Something is already selected. Don't do anything.");
+        if (window.getSelection().toString()) {
+            write_to_console("Something is already selected. Don't do anything.")
         } else {
-            write_to_console("Nothing is selected.");
+            write_to_console("Nothing is selected.")
 
-            linkAddress.text($(this).prop('href'));
-            write_to_console("linkAddress: " + linkAddress.text());
-            selectElement(linkAddress);
+            let targetHref = $(this).prop('href')
+            if (targetHref.startsWith("http")) {
+                linkAddress.css({position: 'fixed', top: '0em', right: '-9999em'})
+            } else {
+                linkAddress.css({position: 'fixed', top: '0em', right: '0em'})
+            }
+            linkAddress.text(targetHref)
+            write_to_console("linkAddress: " + linkAddress.text())
+            selectElement(linkAddress)
         }
 
-        write_to_console("Current Selection: " + window.getSelection().toString());
-    }).on("mouseleave", "a", function(){
-            // Every time the user leaves a link
-            write_to_console("Leaving link.");
-            clearLinkAddress();
-        });
+        write_to_console("Current Selection: " + window.getSelection().toString())
+    }).on("mouseleave", "a", function () {
+        // Every time the user leaves a link
+        write_to_console("Leaving link.")
+        clearLinkAddress()
+    })
 
-    $(window).on("beforeunload", function() {
-        clearLinkAddress();
-    });
+    $(window).on("beforeunload", function () {
+        clearLinkAddress()
+    })
 
-});
+})
